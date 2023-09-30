059b08835e4d53036007239a7663e856a4d1b14c
LimYeram <yeramyeye@gmail.com>
2023-09-30 19:16:50 +0900

    🍀[FEAT] backjoon 14888

---
+++ BackJoon/DFS/14888.py
@@ -0,0 +1,11 @@
+# 못 풀었으니 다음에 다시 도전!
+# N = 6
+# numbers = [1,2,3,4,5,6]
+# operators = [2,1,1,1]
+
+# # N = input()
+# # numbers = list(map(int, input().split()))
+# # operators = list(map(int, input().split()))
+
+# def getMax():
+#   for i in numbers:
