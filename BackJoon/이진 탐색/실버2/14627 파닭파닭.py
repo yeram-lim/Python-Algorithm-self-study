b56bb609ceb92f2efa59900629023acebf8a8c1b
yeram <ramye@naver.com>
2024-05-24 09:50:17 +0900

    ⚡️[백준] 14627 파닭파닭

--- BackJoon/이진 탐색/실버2/14627 파닭파닭.py
+++ BackJoon/이진 탐색/실버2/14627 파닭파닭.py
@@ -1,44 +1,27 @@
+import sys
+input = sys.stdin.readline
+
 S, C = list(map(int, input().split()))
-pa_lengths = []
+pa_list = []
 for i in range(S):
-    pa_lengths.append(int(input()))
-pa_lengths.sort()
+    pa_list.append(int(input()))
 
-min_pa = pa_lengths[-1] / C
-max_pa = pa_lengths[0]
-if sum(pa_lengths) / C < pa_lengths[0]: #소숫점 주의, 한 파닭에 들어갈 수 있는 최대 파의 길이
-    max_pa = sum(pa_lengths) // C
-# print('min_pa:',min_pa,'max_pa:',max_pa)
-use_pa_len = 0
-while True:
-    def get_pa_dak_count(pa_lengths, target_pa):
-        pa_dak_count = 0
-        for pa in pa_lengths:
-            pa_dak_count += pa // target_pa
-        return pa_dak_count
-    
-    pa_dak_count_with_max = get_pa_dak_count(pa_lengths, max_pa)
-    if pa_dak_count_with_max == C:
-        break
+min_pa = pa_list[-1] / C
+max_pa = pa_list[0]
+if sum(pa_list) / C < pa_list[0]: #소숫점 주의, 한 파닭에 들어갈 수 있는 최대 파의 길이
+    max_pa = sum(pa_list) // C
 
-    # if max_pa - min_pa <= 1:
-    #     pa_dak_count = get_pa_dak_count(pa_lengths, max_pa)
-    #     if pa_dak_count == C:
-    #         use_pa_len = max_pa
-    #     break
-
+while min_pa <= max_pa:
     mid_pa = (min_pa + max_pa) // 2
-    pa_dak_count = get_pa_dak_count(pa_lengths, mid_pa)
-    
-    if pa_dak_count == C:
-        min_pa = mid_pa
-        if use_pa_len < mid_pa:
-            use_pa_len = mid_pa
+    pa_dak_count = 0
+    for pa in pa_list:
+        pa_dak_count += pa // mid_pa
+
     if pa_dak_count < C:
-        max_pa = mid_pa
-    if pa_dak_count > C:
-        min_pa = mid_pa
-print(int(sum(pa_lengths) - (use_pa_len * C)))
+        max_pa = mid_pa - 1
+    if pa_dak_count >= C:
+        min_pa = mid_pa + 1
+print(int(sum(pa_list) - (max_pa * C)))
 
 
 # 3 5
